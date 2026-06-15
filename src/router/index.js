a11c8e771478faa0690a1ef7c86d47f43d3736ec
combonazridine04-hue <combonazridine04@gmail.com>
2026-06-15 13:52:51 +0300

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomeView from '../views/HeroView.vue'
 import AboutView from '../views/AboutView.vue'
@@ -29,7 +29,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
